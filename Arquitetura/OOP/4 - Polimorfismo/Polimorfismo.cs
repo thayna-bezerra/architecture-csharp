9f78fd78efd3b60bc35533629755dea6b7335367
thayna <arraisthayna2106@gmail.com>
2023-08-09 23:37:05 -0300

    class coffeeExpress that inherits from the appliance class - concept of polymorphism

---
+++ Arquitetura/OOP/4 - Polimorfismo/Polimorfismo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Arquitetura.OOP
+{ 
+    public class CafeteiraEspressa : Eletrodomestico
+    {
+        public CafeteiraEspressa(string nome, int voltagem)
+            :base(nome, voltagem) 
+        {
+
+        }
+
+        public CafeteiraEspressa()
+            :base("Padrao", 220) //construtor vazio
+        {
+
+        }
+
+        private static void AquecerAgua()
+        {
+
+        }
+
+        private static void MoerGraos()
+        {
+
+        }
+
+        public void FazerCafe()
+        {
+            AquecerAgua();
+            MoerGraos();
+        }
+
+        public override void Ligar()
+        {
+            //verificar recipiente de água
+        }
+
+        public override void Desligar()
+        {
+            //resfriar aquecedor
+        }
+    }
+}
