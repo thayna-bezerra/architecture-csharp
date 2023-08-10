d61f5c9b0c5673234dd4141d324ba88f286d1372
thayna <arraisthayna2106@gmail.com>
2023-08-09 22:59:10 -0300

    abstract class appliance with methods for reuse - concept of abstraction

---
+++ Arquitetura/OOP/3 - Abstracao/Abstracao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Arquitetura.OOP
+{
+    public abstract class Eletrodomestico
+    {
+        private readonly string _nome;
+        private readonly int _voltagem;
+        protected Eletrodomestico(string nome, int voltagem)
+        {
+            _nome = nome;
+            _voltagem = voltagem;
+        }
+
+        //A classe que herdar a classe Eletrodomestico necessariamente precisará implementar os métodos 
+        public abstract void Ligar();
+        public abstract void Desligar();
+    }
+}
