4de8875928a58fc7d168484a1c6910ebf0032c62
thayna <arraisthayna2106@gmail.com>
2023-08-14 16:34:55 -0300

    inheritance vs composition

---
+++ Arquitetura/OOP/6 - Heranca VS Composicao/HerancaVSComposicao.cs
@@ -0,0 +1,18 @@
+﻿namespace Arquitetura.OOP
+{
+    public class PessoaFisica : Pessoa
+    {
+        public string CPF { get; set; }
+    }
+
+    // Composição no lugar de herança
+    public class Cliente
+    {
+        private readonly Pessoa _pessoa;
+
+        public Cliente(Pessoa pessoa)
+        {
+            _pessoa = pessoa;
+        }
+    }
+}
