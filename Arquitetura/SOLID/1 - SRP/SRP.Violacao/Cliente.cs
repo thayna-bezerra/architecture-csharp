57d6dac18be9e871641b75a4a78d82a73ce76c51
thayna <arraisthayna2106@gmail.com>
2023-08-16 15:42:31 -0300

    SOLID single responsibility principle (SRP) violation scenario

---
+++ Arquitetura/SOLID/1 - SRP/SRP.Violacao/Cliente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Net.Mail;
+
+namespace Arquitetura.SOLID.SRP.Violacao
+{
+    public class Cliente
+    {
+        public int ClientId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        //Sobrecarda de responsabilidades
+        public string AdicionarCliente() //Não deveria ser responsabilidade da classe Cliente
+        {
+            //A classe cliente não pode fazer validação de seus proprios dados
+            if (!Email.Contains("@"))
+                return "Cliente com e-mail inválido";
+
+            if (CPF.Length != 11)
+                return "Cliente com CPF inválido";
+
+            using (var cn = new SqlConnection()) //O banco de dados não deveria ser de responsabilidade da classe cliente
+            {
+                var cmd = new SqlCommand();
+
+                cn.ConnectionString = "MinhaConnectionString";
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "INSERT INTO CLIENTE (NOME, EMAIL, CPF)";
+
+                cmd.Parameters.AddWithValue("nome", Nome);
+                cmd.Parameters.AddWithValue("email", Email);
+                cmd.Parameters.AddWithValue("cpf", CPF);
+                cmd.Parameters.AddWithValue("dataCad", DataCadastro);
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+            }
+
+            var mail = new MailMessage("empresa@empresa.com", Email);
+            var client = new SmtpClient
+            {
+                Port = 25,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Host = "smtp.google.com"
+            };
+
+            mail.Subject = "Bem Vindo";
+            mail.Body = "Parabéns! Você está cadastrado.";
+            client.Send(mail);
+
+            return "Cliente cadastrado com sucesso!";
+        }
+    }
+}
