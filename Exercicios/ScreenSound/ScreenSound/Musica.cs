7a3c8bd3c7533669d313d4e2a27815d1a8a60ce1
thayna <arraisthayna2106@gmail.com>
2023-09-08 22:37:48 -0300

    exercise screen sound

---
+++ Exercicios/ScreenSound/ScreenSound/Musica.cs
@@ -0,0 +1,30 @@
+﻿class Musica
+{
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+
+    public string Nome { get; }
+    public Banda Artista { get; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; } //deixa explicito que esta variavel tem operação de leitura/escrita
+    public Genero Genero { get; set; }
+
+
+    public string DescricaoResumida => //so modo leitura
+        $"A música {Nome} pertence à banda {Artista}";
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+
+        if (Disponivel)
+            Console.WriteLine("Disponível no Plano");
+        else
+            Console.WriteLine("Adquira o panlo Plus+");
+    }
+}
---
+++ Exercicios/ScreenSound/ScreenSound/Program.cs
@@ -0,0 +1,24 @@
+﻿Banda linkinPark = new Banda("Linkin Park");
+
+Album albumLinkinPark = new Album("Hybrid Theory");
+
+Musica musica1 = new Musica(linkinPark, "In The End")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(linkinPark, "Crawling")
+{
+    Duracao = 415,
+    Disponivel = false,
+};
+
+albumLinkinPark.AdicionarMusica(musica1);
+albumLinkinPark.AdicionarMusica(musica2);
+linkinPark.AdicionarAlbum(albumLinkinPark);
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+albumLinkinPark.ExibirMusicasDoAlbum();
+linkinPark.ExibirDiscografia();
